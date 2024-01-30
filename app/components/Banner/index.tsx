c066ea944695005e23d403d69253e6111db8ca45
mfazrinizar <anon6372098.id@gmail.com>
2024-01-31 00:38:59 +0700

    Link to oprec

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -17,7 +17,7 @@ const Banner = () => {
                     <div className='grid grid-cols-1 lg:grid-cols-12 my-16'>
                         <div className='arrowTwo'></div>
                         <div className='col-span-12 flex flex-col items-center'>
-                        <div className='col-span-5 lg:-m-48'>
+                            <div className='col-span-5 lg:-m-48'>
                                 <div className='arrowThree'></div>
                                 <div className='arrowFour'></div>
                                 <div className='arrowFive'></div>
@@ -32,7 +32,12 @@ const Banner = () => {
                             </h1>
                             <p className='text-white md:text-lg font-normal mb-10 md:text-start text-center'>Sebuah wadah untuk menampung dan mengeksekusi aspirasi warga Teknik Informatika.</p>
                             <div className='flex align-middle justify-center md:justify-start'>
-                                <button className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton mr-6'>Daftar Sekarang</button>
+                                <button
+                                    className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton mr-6'
+                                    onClick={() => window.location.href = 'https://oprec.hmifunsri.com/daftar'}
+                                >
+                                    Daftar Sekarang
+                                </button>
                                 <button onClick={() => setOpen(true)} className='bg-transparent flex justify-center items-center text-white'><Image src={'/images/Banner/playbutton.svg'} alt="button-image" className='mr-3' width={47} height={47} />Profil</button>
                             </div>
                         </div>
